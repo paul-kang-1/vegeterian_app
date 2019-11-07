e4245dafb6b100b5f59b5e9dbefdbec50de3f7b1
loiteraround <loiter97@gmail.com>
2019-11-07 20:38:56 +0900

    loading/loginscreen
    added loading / loginscreen

--- App.js
+++ App.js
@@ -1,77 +1,56 @@
-import React from 'react';
-import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import MemberScreen from './screen_member/member.js'; // 사용 할려면 매번 import 해와야함 
-import RecoScreen from './screen_reco/reco.js';
-import *  as Font from'expo-font' // 이것도 yarn add 해서 다운 받은거 입니다 
+import React from 'react'; 
+import * as Location from 'expo-location';
+import axios from 'axios';
+import LoadingScreen from "./pages/LoadingScreen";
 
 
-class HomeScreen extends React.Component {
+const MAP_API = '0abbbf5654f34e6dbb2606e6765f5614';
 
-
-
-  componentWillMount() {   // willmount 랑 didmount 의 차이가 있는데 굳이 이해는 안해도 되지만 그냥 render 하기 전에 먼저  실행되는 함수와 그 후에 실행되는 함수라고 알고있음 됩니다 
-    this.loadFonts();
+export default class App extends React.Component{
+  state = {
+    isLoading: true,
+    fontLoaded: false,
+    showLogin: false,
+    toHomeScreen: false
+  }
+  getAddress = async(latitude, longitude) => {
+    const { 
+      data: { results }
+    } = await axios.get(`http://api.opencagedata.com/geocode/v1/json?key=${MAP_API}&q=${latitude}%2C${longitude}&pretty=1`);
+    const comp = results[0].components;
+    if (comp.town){ 
+      address = comp.town;
+    } else if (comp.neighborhood){
+      address = comp.neighborhood;
+    } else if (comp.county) {
+      address = comp.county;
+    } else {
+      address = comp.city;
+    }
+    this.setState({ isLoading: false, address: address});
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getAddress(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Permission denied!")      
+    }
   }
   async loadFonts() {
     await Expo.Font.loadAsync({
-
-
-      'example2-font': require('./assets/fonts/BebasNeue-Regular.ttf'),
-      'example-font': require('./assets/fonts/Bayon.ttf'),  // 폰트 끌어오기 
-      
-
+      'AlegreyaSans-Regular': require('C:/Users/loite/Documents/Projects/Repos/vs-choice/assets/fonts/AlegreyaSans-Regular.ttf')
     });
   }
-
-
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen1</Text>
-        <Button
-          title={"to reco"}
-          onPress={() => this.props.navigation.navigate('Recommend')}
-        />
-
-        <Button
-          title={"to member"}
-          onPress={() => this.props.navigation.navigate('member')} //  버튼 on press 는 누가봐도 알겠고 ㅋㅋ navigation 이 이제 화면간 이동 할때 쓰는건데   this 는 이함수를 가르치는거고 , prop 은 자식 , 잘 모르겠으면 자세한건 만나서 설명해드림
-        />
-        <TouchableOpacity>
-
-          <Image
-            source={require("./assets/plate.png")} //TouchableOpacity  가 이제 버튼인데 안에 컴포넌트로 이미지를 넣으면 내가 원하는 모양의 버튼을 만들수 있는 컴포넌트 이미지 사용법은 이거 처럼  require("./as/as") 이러던가  source={{uri:"www.naver.com"}}
-          ></Image>
-        </TouchableOpacity>
-
-
-
-
-      </View>
-    );
+  componentDidMount(){
+    this.getLocation();
+    this.loadFonts();
+  }
+  render(){
+    const { isLoading } = this.state;
+    return isLoading? <LoadingScreen/>: <LoadingScreen showLogin={true}/>;
   }
 }
-
-
-
-// 총 몇가지 종류의 화면이 있는지 설정하는 부분 aka 대분류 
-// 일단 크게 홈화면, 그리고 음식점 추천 화면 그리고 개인 계정 부분이 있다고 설정하였음
-//  ooscreen 은 컴포넌트로서 하나의 js 에 하나 씩 밖에 존재할수 없음 
-//  위에 import 부분 보면 다른 파일에서 설정한 reco 와 member  가 있음 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
-  Recommend: {
-    screen: RecoScreen,
-  },
-  member: {
-    screen: MemberScreen,
-  },
-
-});
-
-export default createAppContainer(AppNavigator); 
---
+++ pages/LoadingScreen.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image, ActivityIndicator } from 'react-native';  
+import firebase from 'firebase';
+import { firebaseConfig } from '../config';
+import HomeScreen from './HomeScreen';
+import * as Google from 'expo-google-app-auth';
+
+firebase.initializeApp(firebaseConfig)
+
+export default class LoadingScreen extends React.Component{
+    signInWithGoogleAsync = async () => {
+        try {
+          const result = await Google.logInAsync({
+            behavior: 'web',
+            androidClientId: '386677764344-4f2lfkvaupm96uih0rg1afr5qh4o1fb0.apps.googleusercontent.com',
+            iosClientId: '386677764344-oeoovf93253f509banm2rgl32l54fkj0.apps.googleusercontent.com',
+            scopes: ['profile', 'email'],
+          });
+      
+          if (result.type === 'success') {
+            return result.accessToken;
+          } else {
+            return { cancelled: true };
+          }
+        } catch (e) {
+          return { error: true };
+        }
+    }
+      
+    state = { toHomeScreen: false }
+    componentDidMount(){
+        this.checkIfLoggedIn();
+    }
+    checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged(user =>{
+            if(user) {
+                this.setState({ toHomeScreen: true })                         
+            } else {
+                this.setState({ toHomeScreen: false }) 
+            }
+        })
+    }
+
+    render(){
+        const { toHomeScreen } = this.state
+        return(
+            toHomeScreen? <HomeScreen/>
+            :   <View style={styles.container}>
+                    <View style={styles.backgroundContainer}>
+                        <Image style={styles.backgroundImage} source={require('../assets/images/loading_bg.png')}/>
+                    </View>
+                    <View style={styles.backgroundContainer}>
+                        <Image style={styles.logo} source={require('../assets/images/app_title.png')}/>
+                    </View>
+                    {this.props.showLogin?
+                        <React.Fragment>
+                            <View style={{flex: 1}}/>
+                            <View style={{flex: 1, justifyContent: "center"}}>
+                                <Text  style={styles.loginText}>Log in With:</Text>
+                                <Image style={styles.loginButton} source={require('../assets/images/facebook_login.png')}/>
+                                <Button title='Sign in with Google' 
+                                onPress={()=> this.signInWithGoogleAsync()}/>
+                            </View>
+                        </React.Fragment>
+                    :   <React.Fragment>  
+                            <View style={{flex: 1}}/>
+                            <View style={{flex: 1, justifyContent: "center"}}>
+                                <ActivityIndicator size="large"/>
+                            </View>
+                        </React.Fragment> 
+                    }
+                </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    backgroundContainer: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%'
+    },
+    overlay: {
+        justifyContent: "center",
+        //alignItems: "center",
+    },
+    logo: {
+        alignSelf: "center",
+        flex: 1,
+        aspectRatio: 1/4,
+        resizeMode: "contain"
+    },
+    loginButton: {
+        marginBottom: 15
+    },
+    loginText: {
+        color: "white",
+        alignSelf: "center",
+        marginBottom: 15,
+        fontSize: 18,
+        fontFamily: 'AlegreyaSans-Regular', 
+    }
+})
