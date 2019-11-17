f0f0ec72396f99029949232cf2653f61cfbdd6e4
loiteraround <loiter97@gmail.com>
2019-11-17 13:49:51 +0900

    hooks and fixes
    React Hooks 적용, 디자인 요소 수정

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,25 +1,57 @@
-import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import axios from "axios";
+import * as Location from "expo-location";
 
-class HomeScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>homescreen</Text>
-            </View>
-        );
+const MAP_API = "0abbbf5654f34e6dbb2606e6765f5614";
+
+const HomeScreen = () => {
+  const [address, setAddress] = useState("");
+  const getAddress = async (latitude, longitude) => {
+    const {
+      data: { results }
+    } = await axios.get(
+      `http://api.opencagedata.com/geocode/v1/json?key=${MAP_API}&q=${latitude}%2C${longitude}&pretty=1`
+    );
+    const comp = results[0].components;
+    if (comp.town) {
+      loc = comp.town;
+    } else if (comp.neighborhood) {
+      loc = comp.neighborhood;
+    } else if (comp.county) {
+      loc = comp.county;
+    } else {
+      loc = comp.city;
     }
-}
+    setAddress(loc);
+  };
+  const getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      getAddress(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Permission denied!");
+    }
+  };
+  useEffect(() => {
+    getLocation();
+  });
+  return (
+    <View style={styles.container}>
+      <Text>{address}</Text>
+      <Text>location is the following</Text>
+    </View>
+  );
+};
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- pages/LoadingScreen.js
+++ pages/LoadingScreen.js
@@ -1,187 +1,237 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Image, ActivityIndicator, TouchableOpacity } from 'react-native';  
-import firebase, { googleProvider } from '../firebase';
-import { firebaseConfig } from '../config';
-import HomeScreen from './HomeScreen';
-import * as Google from 'expo-google-app-auth';
-import * as Facebook from 'expo-facebook';
-import Svg from 'react-native-svg';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity
+} from "react-native";
+import firebase, { googleProvider } from "../firebase";
+import HomeScreen from "./HomeScreen";
+import * as Google from "expo-google-app-auth";
+import * as Facebook from "expo-facebook";
 
-export default class LoadingScreen extends React.Component{
-  signInWithFacebook = async () => {
-    const { navigation } = this.props;
+const LoadingScreen = ({ showLogin }) => {
+  const [toHomeScreen, setHomescreen] = useState(false);
+  const signInWithFacebook = async () => {
     try {
       const {
         type,
         token,
         expires,
         permissions,
-        declinedPermissions,
-      } = await Facebook.logInWithReadPermissionsAsync('1998302263810491', {
-        permissions: ['public_profile','email'],
+        declinedPermissions
+      } = await Facebook.logInWithReadPermissionsAsync("1998302263810491", {
+        permissions: ["public_profile", "email"]
       });
-      if (type === 'success') {
+      if (type === "success") {
         // Get the user's name using Facebook's Graph API
         // const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
         const response = await fetch(
-        `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
-        );          
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
+        );
         const responseJSON = JSON.stringify(await response.json());
         var obj = JSON.parse(responseJSON);
-        alert(obj.name);   
+        console.log(obj.name);
+        setHomescreen(true);
       } else {
         // type === 'cancel'
       }
     } catch ({ message }) {
       console.log(`Facebook 1 Error: ${message}`);
-      alert("fisrt ${message}");
-    }  
-  }
+      alert("first ${message}");
+    }
+  };
 
-  isUserEqual = (googleUser, firebaseUser) => {
+  const isUserEqual = (googleUser, firebaseUser) => {
     if (firebaseUser) {
       var providerData = firebaseUser.providerData;
-        for (var i = 0; i < providerData.length; i++) {
-          if (providerData[i].providerId === googleProvider.PROVIDER_ID &&
-            providerData[i].uid === googleUser.getBasicProfile().getId()) {
-            // We don't need to reauth the Firebase connection.
-              return true;
-            }
-          }
+      for (var i = 0; i < providerData.length; i++) {
+        if (
+          providerData[i].providerId === googleProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()
+        ) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
     }
     return false;
-  }
-  onSignIn = googleUser => {
+  };
+
+  const onSignIn = googleUser => {
     //console.log('Google Auth Response', googleUser);
     // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-      unsubscribe();
-      // Check if we are already signed-in Firebase with the correct user.
-      if (!this.isUserEqual(googleUser, firebaseUser)) {
-        // Build Firebase credential with the Google ID token.
-        const credential = googleProvider.credential(
-          googleUser.idToken, googleUser.accessToken);
-        // Sign in with credential from the Google user.
-        firebase.auth().signInWithCredential(credential)
-          .then(function(){console.log('User Signed In');})
-          .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-          });
+    var unsubscribe = firebase
+      .auth()
+      .onAuthStateChanged(function(firebaseUser) {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          const credential = googleProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken
+          );
+          // Sign in with credential from the Google user.
+          firebase
+            .auth()
+            .signInWithCredential(credential)
+            .then(function() {
+              setHomescreen(true);
+            })
+            .catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
         } else {
-          console.log('User already signed-in Firebase.');
+          console.log("User already signed-in Firebase.");
         }
-    }.bind(this));
-  }
+      });
+  };
 
-  signInWithGoogleAsync = async () => {
+  const signInWithGoogleAsync = async () => {
     try {
       const result = await Google.logInAsync({
-        androidClientId: '386677764344-4f2lfkvaupm96uih0rg1afr5qh4o1fb0.apps.googleusercontent.com',
-        iosClientId: '386677764344-oeoovf93253f509banm2rgl32l54fkj0.apps.googleusercontent.com',
-        scopes: ['profile', 'email'],
+        androidClientId:
+          "386677764344-4f2lfkvaupm96uih0rg1afr5qh4o1fb0.apps.googleusercontent.com",
+        iosClientId:
+          "386677764344-oeoovf93253f509banm2rgl32l54fkj0.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
       });
-    
-      if (result.type === 'success') {
-        this.onSignIn(result);
+
+      if (result.type === "success") {
+        onSignIn(result);
         return result.accessToken;
       } else {
         return { cancelled: true };
       }
-      } catch (e) {
-        return { error: true };
-      }
-  }
-    
-  state = { toHomeScreen: false }
-  componentDidMount(){
-    this.checkIfLoggedIn();
-  }
-  checkIfLoggedIn = () => {         
-      if( firebase.auth().currentUser!=null) {
-      this.setState({ toHomeScreen: true })                         
-      } else {
-      this.setState({ toHomeScreen: false }) 
-      }         
-  }
+    } catch (e) {
+      return { error: true };
+    }
+  };
 
-  render(){
-    const { toHomeScreen } = this.state
-    return(
-      toHomeScreen? <HomeScreen/>
-      : <View style={styles.container}>
-          <View style={styles.backgroundContainer}>
-            <Image style={styles.backgroundImage} source={require('../assets/images/loading_bg.png')}/>
+  useEffect(() => {
+    checkIfLoggedIn();
+  });
+
+  const checkIfLoggedIn = () => {
+    if (firebase.auth().currentUser != null) {
+      setHomescreen(true);
+    } else {
+      setHomescreen(false);
+    }
+  };
+
+  return toHomeScreen ? (
+    <HomeScreen />
+  ) : (
+    <View style={styles.container}>
+      <View style={styles.backgroundContainer}>
+        <Image
+          style={styles.backgroundImage}
+          source={require("../assets/images/loading_bg.png")}
+        />
+      </View>
+      <View style={styles.backgroundContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/images/app_title.png")}
+        />
+      </View>
+      {showLogin ? (
+        <React.Fragment>
+          <View style={{ flex: 1 }} />
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Text style={styles.loginText}>Start With:</Text>
+            <TouchableOpacity
+              style={styles.loginButtonContainer}
+              onPress={() => signInWithFacebook()}
+            >
+              <Image
+                style={styles.LoginButton}
+                resizeMode={"contain"}
+                source={require("../assets/images/facebook_login.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.loginButtonContainer}
+              onPress={() => signInWithGoogleAsync()}
+            >
+              <Image
+                style={styles.LoginButton}
+                source={require("../assets/images/google_login.png")}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={styles.backgroundContainer}>
-            <Image style={styles.logo} source={require('../assets/images/app_title.png')}/>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <View style={{ flex: 1 }} />
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <ActivityIndicator size="large" />
           </View>
-            {this.props.showLogin?
-              <React.Fragment>
-                <View style={{flex: 1}}/>
-                <View style={{flex: 1, justifyContent: "center"}}>
-                  <Text  style={styles.loginText}>Log in With:</Text>
-                    <TouchableOpacity                             
-                      onPress={()=> this.signInWithFacebook()}>
-                      <Image style={styles.loginButton} source={require('../assets/images/facebook_login.png')}/>
-                    </TouchableOpacity>
-                    <Button title='Sign in with Google' 
-                      onPress={()=> this.signInWithGoogleAsync()}/>
-                </View>
-              </React.Fragment>
-            : <React.Fragment>  
-                <View style={{flex: 1}}/>
-                <View style={{flex: 1, justifyContent: "center"}}>
-                  <ActivityIndicator size="large"/>
-                </View>
-              </React.Fragment> 
-          }
-        </View>
-    );
-  }
-}
+        </React.Fragment>
+      )}
+    </View>
+  );
+};
+
+export default LoadingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems:"center",
-        justifyContent:"center"
-    },
-    backgroundContainer: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-    },
-    backgroundImage: {
-        width: '100%',
-        height: '100%'
-    },
-    overlay: {
-        justifyContent: "center",
-        //alignItems: "center",
-    },
-    logo: {
-        alignSelf: "center",
-        flex: 1,
-        aspectRatio: 1/4,
-        resizeMode: "contain"
-    },
-    loginButton: {
-        marginBottom: 15
-    },
-    loginText: {
-        color: "white",
-        alignSelf: "center",
-        marginBottom: 15,
-        fontSize: 18,
-        fontFamily: 'AlegreyaSans-Regular', 
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  backgroundContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%"
+  },
+  logo: {
+    alignSelf: "center",
+    flex: 1,
+    aspectRatio: 1 / 4,
+    resizeMode: "contain"
+  },
+  LoginButton: {
+    maxWidth: "100%",
+    maxHeight: "auto",
+    resizeMode: "contain"
+  },
+  loginText: {
+    color: "white",
+    //alignSelf: "center",
+    marginTop: 30,
+    marginBottom: 15,
+    fontSize: 18,
+    fontFamily: "AlegreyaSans-Regular"
+  },
+  loginButtonContainer: {
+    maxWidth: "75%",
+    maxHeight: "15%",
+    justifyContent: "center",
+    marginBottom: 15
+  }
+});
