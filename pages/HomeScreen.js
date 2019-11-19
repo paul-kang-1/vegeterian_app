201f7dcf881804335faa1dd2de012a43dc7b74a9
park-mk <whezme@naver.com>
2019-11-19 13:04:30 +0900

    flatlist2

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,73 +1,86 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    FlatList,
-     TouchableOpacity ,
-     Image
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+  Image
 } from "react-native";
-import  firebase,{storage}  from "../firebase";
+import firebase, { storage } from "../firebase";
 
 class HomeScreen extends Component {
 
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-    
-          datasource: [],
-       
-        };
-      }
-componentDidMount() {
-    
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+      datasource: [],
+
+    };
+  }
+  componentDidMount() {
+
     this.makeRemoteRequest();
   }
 
   makeRemoteRequest = () => {
-    
-     
 
- 
+
+
+
 
     var usersRef = firebase.database().ref('resaturant');
-    
-    
-usersRef.on('value', (snapshot) => {
-    
-    
-     var m=snapshot.val() 
-     var keys= Object.values(m);
-    
-  this.setState({
-    datasource:  keys
-  })
-});
-}
-renderItem =({item})=>{
 
-    return(
+
+    usersRef.on('value', (snapshot) => {
+
+
+      var m = snapshot.val()
+      var keys = Object.values(m);
+
+      this.setState({
+        datasource: keys
+      })
+    });
+  }
+  renderItem = ({ item }) => {
+    let dimensions = Dimensions.get("window");
+    let imageheight = 2 * dimensions.height / 14
+    let imagewidth = dimensions.width - 30;
+    return (
       <TouchableOpacity
-      onPress={() => {}
+        style={{ marginTop: 20 }}
+        onPress={() => { }
 
-    
-     
-   
-   }
+
+
+
+        }
       >
-     <View  style={{  flex:1,  marginLeft:10,marginBottom:6,borderColor:'black'}} >
-             <Image  style={styles.icon}
-                   source={{uri:item.image}}
-            
-            />
-             <View  >
-       <Text>{item.name}</Text>
-             </View>
-            
-           </View>
-           </TouchableOpacity>   
+        <ImageBackground style={{
+          height: imageheight, width: imagewidth,
+        }}
+          imageStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+          source={{ uri: item.image }}>
+          <View style={{ flex: 1, marginLeft: 10, marginBottom: 6, borderColor: 'black' }} >
+
+
+
+
+
+          </View>
+        </ImageBackground>
+        <View style={{ borderBottomWidth: 2, borderLeftWidth: 1, borderRightWidth: 1, borderBottomLeftRadius: 20, borderBottomRightRadius: 20, height: imageheight / 2 }}>
+          <View  >
+            <Text>{item.name}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
 
 
 
@@ -76,45 +89,45 @@ renderItem =({item})=>{
 
 
 
-}
-    
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>homescreen</Text>
-                <FlatList 
-     
-     data={this.state.datasource}
-     
-     renderItem={this.renderItem}
-     
-    
-     keyExtractor={item => item.name}
-     initialNumToRender={4}
-     maxToRenderPerBatch={4}
- 
-     onRefresh={this.handleRefresh}
-     refreshing={this.state.refreshing}
-  
-     onEndReachedThreshold={10000000} 
-      
-  
-   />
+  }
 
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ marginTop: 200 }}>homescreen</Text>
+        <FlatList
+
+          data={this.state.datasource}
+
+          renderItem={this.renderItem}
+
+
+          keyExtractor={item => item.name}
+          initialNumToRender={4}
+          maxToRenderPerBatch={4}
+
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+
+          onEndReachedThreshold={10000000}
+
+
+        />
+
+      </View>
+    );
+  }
 }
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    icon:{
-        height:70,
-        width:70
-    },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  icon: {
+    height: 70,
+    width: 70
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
