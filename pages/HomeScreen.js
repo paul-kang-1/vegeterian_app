2cf526d0d9800767c05d0fbee196a957b8928d73
loiteraround <loiter97@gmail.com>
2019-11-28 19:27:39 +0900

    enhanced design elements

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -5,17 +5,21 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
-  Image
+  Image,
+  ScrollView
 } from "react-native";
 import axios from "axios";
 import * as Location from "expo-location";
 import firebase, { storage } from "../firebase";
+import Constants from "expo-constants";
+import { black } from "ansi-colors";
 
 const MAP_API = "0abbbf5654f34e6dbb2606e6765f5614";
 
 const HomeScreen = () => {
   const [address, setAddress] = useState("");
   const [dataSource, setDataSource] = useState([]);
+  const [scrollEnabled, setScroll] = useState(false);
 
   const getAddress = async (latitude, longitude) => {
     console.log("getaddress");
@@ -67,7 +71,7 @@ const HomeScreen = () => {
           console.log("running");
         }}
       >
-        <View style={styles.imageCard}>
+        <View style={[styles.imageCard, styles.shadow]}>
           <Image style={styles.thumbnail} source={{ uri: item.image }} />
           <View>
             <Text style={styles.restaurantTitle}>{item.name}</Text>
@@ -85,83 +89,112 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.backgroundContainer}>
-        <Image
-          style={styles.domaImage}
-          source={require("../assets/images/doma.png")}
-        />
-      </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          width: "100%",
-          paddingBottom: 20,
-          justifyContent: "space-between",
-          paddingTop: 60,
-          paddingLeft: 20
-        }}
+      <ScrollView
+        style={{ flex: 1, maxWidth: "100%" }}
+        //contentContainerStyle={{height:350, maxWidth: "100%" }}
       >
-        <Text style={styles.pageTitle}>{"V's Pick"}</Text>
-        <TouchableOpacity>
-          
-        </TouchableOpacity>
-      </View>
-      <View style={{ flex: 10 }}>
-        <FlatList
-          data={dataSource}
-          renderItem={renderItem}
-          keyExtractor={item => item.name}
-          initialNumToRender={4}
-          maxToRenderPerBatch={4}
-          //onRefresh={this.handleRefresh}
-          //refreshing={this.state.refreshing}
-          //onEndReachedThreshold={10000000}
-        />
-      </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.pageTitle}>{"V's Pick"}</Text>
+          <Image
+            style={{ alignSelf: "flex-end" }}
+            source={require("../assets/images/doma.png")}
+          />
+        </View>
+        <View style={[styles.vsPick, styles.shadow]}>
+          <Text>{address}</Text>
+        </View>
+        <View style={styles.titleContainer2}>
+          <Text style={styles.pageTitle}>{"Around You"}</Text>
+        </View>
+        <View style={{ height: "100%" }}>
+          <FlatList
+            data={dataSource}
+            renderItem={renderItem}
+            keyExtractor={item => item.name}
+            initialNumToRender={3}
+            maxToRenderPerBatch={3}
+            scrollEnabled={scrollEnabled}
+            //onRefresh={this.handleRefresh}
+            //refreshing={this.state.refreshing}
+            //onEndReachedThreshold={10000000}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 export default HomeScreen;
 
+const elevationShadowStyle = elevation => {
+  return {
+    elevation,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0.5 * elevation },
+    shadowOpacity: 0.3,
+    shadowRadius: 0.8 * elevation
+  };
+};
+
 const styles = StyleSheet.create({
+  shadow: elevationShadowStyle(4),
+  vsPick: {
+    height: 400,
+    marginHorizontal: 15,
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    backgroundColor: "white",
+    justifyContent: "center",
+    borderRadius: 8
+  },
+  titleContainer: {
+    height: 123,
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingTop: 60,
+    paddingLeft: 20,
+    marginBottom: 5
+  },
+  titleContainer2: {
+    flex: 0,
+    flexDirection: "row",
+    width: "100%",
+    paddingTop: 20,
+    justifyContent: "space-between",
+    paddingLeft: 20
+  },
   pageTitle: {
     fontFamily: "El-Messiri-SemiBold",
-    fontSize: 45
-  },
-  backgroundContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0
-  },
-  domaImage: {
+    fontSize: 45,
     alignSelf: "flex-end"
+    //marginBottom:10
   },
   thumbnail: {
-    width: 300,
-    height: 300
+    //flex: 1,
+    width: null,
+    height: 170,
+    resizeMode: "cover"
   },
   imageCard: {
     flex: 1,
-    marginLeft: 10,
-    marginBottom: 6,
-    borderColor: "black",
-    borderWidth: 4
+    height: 250,
+    marginHorizontal: 15,
+    marginBottom: 20,
+    backgroundColor: "white",
+    borderRadius: 8,
+    overflow: "hidden"
   },
   container: {
     flex: 1,
+    width: "100%",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#EAEAE8"
   },
-  addressText: {
-    //paddingTop: 30,
-    fontSize: 25,
-    fontFamily: "OpenSans-SemiBold"
-  },
   restaurantTitle: {
     fontSize: 20,
-    fontFamily: "OpenSans-SemiBold"
+    fontFamily: "OpenSans-SemiBold",
+    paddingVertical: 10,
+    marginLeft: 10
   }
 });
