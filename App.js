865b2f74d92756866f5f4c532d798e0d0376e23f
loiteraround <loiter97@gmail.com>
2019-12-05 20:03:20 +0900

    Minor Changes

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import LoadingScreen from "./pages/LoadingScreen";
-import * as Font from 'expo-font';
+import SelectionScreen from "./pages/SelectionScreen";
+import * as Font from "expo-font";
+import { NavigationNativeContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from "./pages/HomeScreen";
+
+const Stack = createStackNavigator();
 
 const App = () => {
   const [isLoading, setLoading] = useState(true);
@@ -11,12 +17,19 @@ const App = () => {
       "OpenSans-Regular": require("./assets/fonts/OpenSans-Regular.ttf"),
       "El-Messiri-SemiBold": require("./assets/fonts/ElMessiri-SemiBold.ttf")
     });
-    setLoading(false)
-  }
+    setLoading(false);
+  };
   useEffect(() => {
-    loadFonts()
-  }, [])
-  return <LoadingScreen showLogin = {!isLoading} />
-}
+    loadFonts();
+  }, []);
+  //return <LoadingScreen showLogin = {!isLoading} />
+  return (
+    <NavigationNativeContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationNativeContainer>
+  );
+};
 
 export default App;
